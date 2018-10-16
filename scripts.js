d2e9f5ec16b746f22d5260d27630b95e9838ea0e
jessicabarnett8219 <jessica.barnett8219@gmail.com>
2018-10-16 14:28:02 -0500

    Refactore ex. 6 and 7 to use booleans rather than === undefined

--- scripts.js
+++ scripts.js
@@ -4423,36 +4423,36 @@ let totalCommits = 0;
 
   // How many of each event type are there? Total Push Events: 11, Total Pull Requests: 7, Total Delete Events: 4, Total Issue Comments: 4, Total Create Events: 4
 
-// let totalPush = 0;
-// let totalPullRequest = 0;
-// let totalDelete = 0;
-// let totalIssueComment = 0;
-// let totalCreate = 0;
+let totalPush = 0;
+let totalPullRequest = 0;
+let totalDelete = 0;
+let totalIssueComment = 0;
+let totalCreate = 0;
 
 
-// for (i = 0; i < githubData.length; i++) {
-//   if (githubData[i].type === "PushEvent") {
-//     totalPush += 1;
-//   } else if (githubData[i].type === "PullRequestEvent") {
-//     totalPullRequest += 1;
-//   } else if (githubData[i].type === "DeleteEvent") {
-//     totalDelete += 1;
-//   } else if (githubData[i].type === "IssueCommentEvent") {
-//     totalIssueComment += 1;
-//   } else if (githubData[i].type === "CreateEvent") {
-//     totalCreate += 1;
-//   } 
-// }
+for (i = 0; i < githubData.length; i++) {
+  if (githubData[i].type === "PushEvent") {
+    totalPush += 1;
+  } else if (githubData[i].type === "PullRequestEvent") {
+    totalPullRequest += 1;
+  } else if (githubData[i].type === "DeleteEvent") {
+    totalDelete += 1;
+  } else if (githubData[i].type === "IssueCommentEvent") {
+    totalIssueComment += 1;
+  } else if (githubData[i].type === "CreateEvent") {
+    totalCreate += 1;
+  } 
+}
 
-// console.log(`Total Push Events: ${totalPush}, Total Pull Requests: ${totalPullRequest}, Total Delete Events: ${totalDelete}, Total Issue Comments: ${totalIssueComment}, Total Create Events: ${totalCreate}`)
+console.log(`Total Push Events: ${totalPush}, Total Pull Requests: ${totalPullRequest}, Total Delete Events: ${totalDelete}, Total Issue Comments: ${totalIssueComment}, Total Create Events: ${totalCreate}`)
 
 // // List all Github users who submitted a pull request that was approved by Steve. (CashewRose, MrErin, megducharme, stevebrownlee)
 
-// for (i = 0; i < githubData.length; i++) {
-//   if (githubData[i].type === "PullRequestEvent") {
-//     console.log(githubData[i].payload.pull_request.user.login)
-//   }
-// }
+for (i = 0; i < githubData.length; i++) {
+  if (githubData[i].type === "PullRequestEvent") {
+    console.log(githubData[i].payload.pull_request.user.login)
+  }
+}
 
 // // List all repositories on which Steve had an event, and show how many events were on each one.
 //     // (2) nashville-software-school/client-side-mastery
@@ -4462,26 +4462,28 @@ let totalCommits = 0;
 //     // (2) nss-day-cohort-27/brenda-snack-cake-store
 //     // (10) nashville-software-school/client-side-mastery
 
-//   for (i = 0; i < githubData.length; i++) {
-//     console.log(githubData[i].repo.name)
-//   }
+  for (i = 0; i < githubData.length; i++) {
+    console.log(githubData[i].repo.name)
+  }
 
-//   // Which event had the most number of commits? The 21st Event had the most
+//  Which event had the most number of commits? The 21st Event had the most
 
-//   for (i = 0; i < githubData.length; i++) {
-//     if (githubData[i].payload.commits === undefined) {
-//       console.log(`Event # ${i+1}: No Commits`);
-//     } else {
-//     console.log(`Event #${i+1}: ${githubData[i].payload.commits.length}`)
-//     }
-//   }
+  for (i = 0; i < githubData.length; i++) {
+    if (githubData[i].payload.commits) {
+      console.log(`Event #${i+1}: ${githubData[i].payload.commits.length}`)
+      } else if (githubData[i].payload.pull_request) {
+        console.log(`Event #${i+1}: ${githubData[i].payload.pull_request.commits}`)
+      } else {
+      console.log(`Event # ${i+1}: No Commits`);
+    }
+  }
 
 //   // Which programming langugages were affected by Steve's events? Which were affected most? JavaScript and Python. JavaScript the most.
 
-//   for (i = 0; i < githubData.length; i++) {
-//     if (githubData[i].payload.pull_request === undefined) {
-//       continue;
-//     } else {
-//     console.log(githubData[i].payload.pull_request.head.repo.language)
-//     }
-//   }
+  // for (i = 0; i < githubData.length; i++) {
+  //   if (githubData[i].payload.pull_request === undefined) {
+  //     continue;
+  //   } else {
+  //   console.log(githubData[i].payload.pull_request.head.repo.language)
+  //   }
+  // }
